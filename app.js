92aecad8facb823874cf0a4b4e72099280fdc6eb
ivan23nug-cell <ivan23nug@gmail.com>
2026-09-20 14:45:11 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzy2cwo5k47KcWev9-_Nqejo-f4o-fjoG9Y",
+  apiKey: "AIzaSyAzy2cwo5k47KcV9-_Nqejo-f4o-fjoG9Y",
   authDomain: "blast-poin-hrr-253dc.firebaseapp.com",
   projectId: "blast-poin-hrr-253dc",
   storageBucket: "blast-poin-hrr-253dc.firebasestorage.app",
